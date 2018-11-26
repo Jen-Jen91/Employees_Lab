9247429cc13485b7a9ad547857dd0dcecdb7aecb
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-11-26 15:49:28 +0000

    Added Employee parent class

--- src/main/java/Staff/Employee.java
+++ src/main/java/Staff/Employee.java
@@ -1,4 +1,35 @@
 package Staff;
 
 public class Employee {
+
+    private String name;
+    private int NINumber;
+    private double salary;
+
+    public Employee(String name, int NINumber, double salary) {
+        this.name = name;
+        this.NINumber = NINumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getNINumber() {
+        return this.NINumber;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void raiseSalary(double raise) {
+        this.salary += raise;
+    }
+
+    public double payBonus() {
+        return this.salary/100;
+    }
+
 }
