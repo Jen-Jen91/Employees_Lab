f4ce6e123664842128bd28c0257bcf2a5ccd78bf
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-11-26 16:17:47 +0000

    Added Director class - all tests passing

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,52 @@
+import Staff.Management.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void before() {
+        director = new Director("Renholm", "NI112233C", 50000, "Head Management", 250000);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Renholm", director.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("NI112233C", director.getNINumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(50000, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void hasDepartment() {
+        assertEquals("Head Management", director.getDeptName());
+    }
+
+    @Test
+    public void hasBudget() {
+        assertEquals(250000, director.getBudget(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(5000);
+        assertEquals(55000, director.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetPayBonus() {
+        assertEquals(500, director.payBonus(), 0.01);
+    }
+
+}
